659a307682e02700be832a8528ded50b56633a9e
Adam Shechter <adam.s.develop@gmail.com>
2016-05-03 00:12:19 -0700

    finished making a random img selector

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
-var main = function () {
-	$("img").click(function() {
-		var altsrc1 = $(this).attr('data-alt-src');
-		// console.log(altsrc1);
-		var altsrctemp1 = $(this).attr('src');
-		$(this).attr('src', altsrc1);
-		$(this).attr('data-alt-src', altsrctemp1);
-	})
+function changeImage (imgN) {
+	var $imageName = $(imgN);
+	var altsrc1 = $imageName.attr('data-alt-src');
+	var altsrctemp1 = $imageName.attr('src');
+	$imageName.attr('src', altsrc1);
+	$imageName.attr('data-alt-src', altsrctemp1);
 }
 
+function randomImg () {
+	for (var i=1; i <= 15; i++) {
+		var num1 = Math.floor ((Math.random() * 5) + 1);
+		console.log("number = " + num1);
+		var str1 = "#img" + num1.toString();	
+		changeImage(str1);
+	}
+}
+
+var main = function () 
+{
+
+	randomImg();
+
+	$("img").click( function () {
+		if ($(this).is("#img1")) {
+			changeImage("#img1");
+		}
+		else if ($(this).is("#img2")) {
+			changeImage("#img2");
+		}
+		else if ($(this).is("#img3")) {
+			changeImage("#img3");
+		}
+		else if ($(this).is("#img4")) {
+			changeImage("#img4");
+		}
+		else if ($(this).is("#img5")) {
+			changeImage("#img5");
+		};
+		
+	});
+
+
+
+}
 
 
 $(document).ready(main());
